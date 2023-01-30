252c43036dd2056eeae6fbe7ee47ab767965a3c3
Bekzod <bekzodbaratov334@gmail.com>
2023-01-30 23:25:28 +0500

    end

--- src/view/List.vue
+++ src/view/List.vue
@@ -2,10 +2,10 @@
   <div class="flex flex-col items-center justify-center mt-10">
     <div>
       <div class="bg-white p-8 rounded-lg">
-        <p class="max-w-[30rem]">{{ list.list }}</p>
+        <p class="max-w-[30rem] min-w-[20rem]">{{ list.list }}</p>
       </div>
       <div class="flex gap-2 py-3">
-        <button @click="" class="btn text-lg bg-danger">
+        <button @click="() => delList()" class="btn text-lg bg-danger">
           <i class="fas fa-trash"></i>
         </button>
         <RouterLink :to="'/list/edit/' + id" class="btn text-lg bg-primary">
@@ -18,16 +18,24 @@
 
 <script setup>
 import { onMounted, ref } from "vue";
-import { useRoute } from "vue-router";
-import { getOneList } from "../plugins/axiosRequsets";
+import { useRoute, useRouter } from "vue-router";
+import { getOneList, delLists } from "../plugins/axiosRequsets";
 
 const route = useRoute();
+const router = useRouter();
 const id = route.params.id;
 
 const list = ref({});
+async function delList() {
+  try {
+    await delLists(id);
+    router.push("/");
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 onMounted(async () => {
   list.value = await getOneList(id);
-  console.log(list.value);
 });
 </script>
--- src/view/listEdit.vue
+++ src/view/listEdit.vue
@@ -1,1 +1,46 @@
-<template>edit list</template>
+<template>
+  <div class="flex flex-col items-center justify-center mt-10">
+    <form class="space-y-3">
+      <div class="bg-white p-6 rounded-lg">
+        <textarea
+          class="outline-none max-w-[30rem] min-w-[20rem] h-full border border-gray-300 p-2 rounded-md"
+          type="text"
+          v-model="list"
+          name="name"
+          id="name"
+        >
+        </textarea>
+      </div>
+      <button @click.prevent="handleForm" class="btn bg-green-500">save</button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import { getOneList, patchList } from "../plugins/axiosRequsets";
+const route = useRoute();
+const router = useRouter();
+const id = route.params.id;
+
+const list = ref("");
+
+onMounted(async () => {
+  try {
+    const res = await getOneList(id);
+    list.value = res.list;
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+async function handleForm() {
+  try {
+    await patchList({ list: list.value }, id);
+    router.push("/");
+  } catch (e) {
+    console.log(e);
+  }
+}
+</script>
