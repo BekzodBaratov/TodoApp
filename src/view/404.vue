da60eaa08a96b090c0d4273164c928d40ddf86e1
Bekzod <bekzodbaratov334@gmail.com>
2023-02-10 22:53:24 +0500

    pageNotFound

--- src/view/404.vue
+++ src/view/404.vue
@@ -1,1 +1,7 @@
-<template>Page not found</template>
+<template>
+  <div class="flex items-center font-bold md:font-semibold justify-center flex-col pt-8 uppercase">
+    <h1 class="text-4xl md:text-6xl py-3 text-gray-800">Page not found</h1>
+    <p class="text-6xl text-gray-800">404</p>
+    <RouterLink class="btn bg-primary mt-6" to="/">Go to Home</RouterLink>
+  </div>
+</template>
